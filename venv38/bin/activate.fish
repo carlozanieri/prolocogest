df702593f454b7f903d73b7ccff7660ff9c5a86a
carlozanieri <carlo.zanieri@gmail.com>
2020-04-15 01:15:08 +0200

    Versione 1.0

--- venv38/bin/activate.fish
+++ venv38/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/carlo/AreaComune/pyramid/prolocogest/venv38"
+set -gx VIRTUAL_ENV "/home/carlo/prolocogest/venv38"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
